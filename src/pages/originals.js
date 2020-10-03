1481e5f4962cad74ec2141afe1c0691d6ff3fd63
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-10-03 13:58:20 -0400

    removing unused code

--- src/pages/originals.js
+++ src/pages/originals.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Link } from 'gatsby';
-import Img from 'gatsby-image';
 
 import Layout from '../components/Layout';
 import ShopGrid from '../components/ShopGrid';
 
-import { useAllOriginals, getDefaultProductImage } from "../helpers/products"
+import { useAllOriginals } from "../helpers/products"
 
 export default ({
 
