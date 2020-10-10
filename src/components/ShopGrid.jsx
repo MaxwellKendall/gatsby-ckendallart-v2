519fa4cb99267249efb68ac6f1b06687b8b281d8
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-10-10 14:10:43 -0400

    shopgrid -- show prices on hover

--- src/components/ShopGrid.jsx
+++ src/components/ShopGrid.jsx
@@ -2,11 +2,12 @@ import React, { useRef, useState, useEffect } from 'react';
 import { Link } from 'gatsby';
 import { kebabCase } from 'lodash';
 
-import { getDefaultProductImage } from "../helpers/products"
+import { getDefaultProductImage, getPrettyPrice } from "../helpers/products"
 import Img from './Img';
 
 export default ({
-    products
+    products,
+    onClick = "product-page"
 }) => {
     const imgRef = useRef(null);
     const [titleDimensions, setTitleDimensions] = useState({ width: 300 });
@@ -35,25 +36,27 @@ export default ({
                 .map(({ slug, img, title, variants, priceRange: { low: lowestPrice } }) => {
                     const hasVariantForSale = variants.some((({ availableForSale }) => availableForSale));
                     return (
-                        <Link to={slug} className="p-5 flex flex-col items-center w-full lg:w-1/2">
-                            <div className="relative">
-                                <Img imgRef={imgRef} responsiveImgs={img} imgName={kebabCase(title)} />
-                                {!hasVariantForSale && (
+                        <>
+                            <Link to={slug} className="p-5 grid-product-img flex flex-col items-center w-full lg:w-1/2">
+                                <div className="relative">
+                                    <Img imgRef={imgRef} responsiveImgs={img} imgName={kebabCase(title)} />
+                                    {!hasVariantForSale && (
+                                        <span
+                                            className="absolute top-0 mt-4 md:mt-12 text-white font-semibold text-center w-24 md:w-48 text-2xl md:text-3xl py-2 left-0 tracking-widest"
+                                            style={{ backgroundColor: "#C097D0", border: "3px solid #8D647A", left: '-.5rem' }}>
+                                            SOLD
+                                        </span>
+                                    )}
                                     <span
-                                        className="absolute top-0 mt-4 md:mt-12 text-white font-semibold text-center w-24 md:w-48 text-2xl md:text-3xl py-2 left-0 tracking-widest"
-                                        style={{ backgroundColor: "#C097D0", border: "3px solid #8D647A", left: '-.5rem' }}>
-                                        SOLD
+                                        className="opacity-0 product-info font-semibold text-base md:text-xl py-5 mb-5 bottom-0 absolute flex flex-wrap items-center justify-center bg-gray-300 tracking-widest text-center w-full"
+                                        style={{ ...titleDimensions, marginBottom: '7px' }}>
+                                            {title.toUpperCase()}
+                                            {hasVariantForSale && variants.length > 1 && <span className="w-full text-center">from {getPrettyPrice(lowestPrice)}</span>}
+                                            {hasVariantForSale && variants.length === 1 && <span className="w-full text-center">{getPrettyPrice(lowestPrice)}</span>}
                                     </span>
-                                )}
-                                <span
-                                    className="opacity-75 font-semibold text-base md:text-xl py-5 mb-5 bottom-0 absolute flex flex-wrap items-center justify-center bg-gray-300 tracking-widest text-center w-full"
-                                    style={{ ...titleDimensions, marginBottom: '7px' }}>
-                                        {title.toUpperCase()}{titleDimensions.width}
-                                        {variants.length > 1 && <span className="w-full text-center">from ${parseInt(lowestPrice, 10).toFixed(2)}</span>}
-                                        {variants.length === 1 && <span className="w-full text-center">${parseInt(lowestPrice, 10).toFixed(2)}</span>}
-                                </span>
-                            </div>
-                        </Link>
+                                </div>
+                            </Link>
+                        </>
                     );
                 })}
         </ul>
