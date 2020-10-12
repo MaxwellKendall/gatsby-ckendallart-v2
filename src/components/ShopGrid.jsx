6531a6bb106025edaa1a5d70ee7c96af4c1bc9c2
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-10-12 14:00:25 -0400

    sqrle layout

--- src/components/ShopGrid.jsx
+++ src/components/ShopGrid.jsx
@@ -29,36 +29,71 @@ export default ({
     }, [imgRef.current])
 
     return (
-        <ul className="flex justify-center md:justify-start align-center items-start flex-wrap">
-            {products
-                .map((product) => ({ ...product, img: getDefaultProductImage(product)}))
-                .filter(({ img }) => img)
-                .map(({ slug, img, title, variants, priceRange: { low: lowestPrice } }) => {
-                    const hasVariantForSale = variants.some((({ availableForSale }) => availableForSale));
-                    return (
-                        <>
-                            <Link to={slug} className="grid-product-img flex flex-col items-center w-full lg:w-1/2 my-4">
-                                <div className="relative">
-                                    <Img imgRef={imgRef} responsiveImgs={img} imgName={kebabCase(title)} />
-                                    {!hasVariantForSale && (
+        <>
+            <ul className="flex flex-col w-full lg:w-1/2">
+                {products
+                    .slice(0, products.length / 2)
+                    .map((product) => ({ ...product, img: getDefaultProductImage(product)}))
+                    .filter(({ img }) => img)
+                    .map(({ slug, img, title, variants, priceRange: { low: lowestPrice } }) => {
+                        const hasVariantForSale = variants.some((({ availableForSale }) => availableForSale));
+                        return (
+                            <li className="my-2">
+                                <Link to={slug} className="grid-product-img flex flex-col items-center w-full">
+                                    <div className="relative">
+                                        <Img imgRef={imgRef} responsiveImgs={img} imgName={kebabCase(title)} />
+                                        {!hasVariantForSale && (
+                                            <span
+                                                className="absolute top-0 mt-4 md:mt-12 text-white font-semibold text-center w-24 md:w-48 text-2xl md:text-3xl py-2 left-0 tracking-widest"
+                                                style={{ backgroundColor: "#C097D0", border: "3px solid #8D647A", left: '-.5rem' }}>
+                                                SOLD
+                                            </span>
+                                        )}
                                         <span
-                                            className="absolute top-0 mt-4 md:mt-12 text-white font-semibold text-center w-24 md:w-48 text-2xl md:text-3xl py-2 left-0 tracking-widest"
-                                            style={{ backgroundColor: "#C097D0", border: "3px solid #8D647A", left: '-.5rem' }}>
-                                            SOLD
+                                            className="hidden md:flex  opacity-0 product-info font-semibold text-base md:text-xl py-5 mb-5 bottom-0 absolute flex-wrap items-center justify-center bg-gray-300 tracking-widest text-center w-full"
+                                            style={{ ...titleDimensions, marginBottom: '7px' }}>
+                                                {title.toUpperCase()}
+                                                {hasVariantForSale && variants.length > 1 && <span className="w-full text-center">from {getPrettyPrice(lowestPrice)}</span>}
+                                                {hasVariantForSale && variants.length === 1 && <span className="w-full text-center">{getPrettyPrice(lowestPrice)}</span>}
                                         </span>
-                                    )}
-                                    <span
-                                        className="hidden md:flex  opacity-0 product-info font-semibold text-base md:text-xl py-5 mb-5 bottom-0 absolute flex-wrap items-center justify-center bg-gray-300 tracking-widest text-center w-full"
-                                        style={{ ...titleDimensions, marginBottom: '7px' }}>
-                                            {title.toUpperCase()}
-                                            {hasVariantForSale && variants.length > 1 && <span className="w-full text-center">from {getPrettyPrice(lowestPrice)}</span>}
-                                            {hasVariantForSale && variants.length === 1 && <span className="w-full text-center">{getPrettyPrice(lowestPrice)}</span>}
-                                    </span>
-                                </div>
-                            </Link>
-                        </>
-                    );
-                })}
-        </ul>
+                                    </div>
+                                </Link>
+                            </li>
+                        );
+                    })}
+            </ul>
+            <ul className="flex flex-col w-full lg:w-1/2">
+                {products
+                    .slice(products.length / 2, products.length)
+                    .map((product) => ({ ...product, img: getDefaultProductImage(product)}))
+                    .filter(({ img }) => img)
+                    .map(({ slug, img, title, variants, priceRange: { low: lowestPrice } }) => {
+                        const hasVariantForSale = variants.some((({ availableForSale }) => availableForSale));
+                        return (
+                            <li className="my-2">
+                                <Link to={slug} className="grid-product-img flex flex-col items-center w-full">
+                                    <div className="relative">
+                                        <Img imgRef={imgRef} responsiveImgs={img} imgName={kebabCase(title)} />
+                                        {!hasVariantForSale && (
+                                            <span
+                                                className="absolute top-0 mt-4 md:mt-12 text-white font-semibold text-center w-24 md:w-48 text-2xl md:text-3xl py-2 left-0 tracking-widest"
+                                                style={{ backgroundColor: "#C097D0", border: "3px solid #8D647A", left: '-.5rem' }}>
+                                                SOLD
+                                            </span>
+                                        )}
+                                        <span
+                                            className="hidden md:flex  opacity-0 product-info font-semibold text-base md:text-xl py-5 mb-5 bottom-0 absolute flex-wrap items-center justify-center bg-gray-300 tracking-widest text-center w-full"
+                                            style={{ ...titleDimensions, marginBottom: '7px' }}>
+                                                {title.toUpperCase()}
+                                                {hasVariantForSale && variants.length > 1 && <span className="w-full text-center">from {getPrettyPrice(lowestPrice)}</span>}
+                                                {hasVariantForSale && variants.length === 1 && <span className="w-full text-center">{getPrettyPrice(lowestPrice)}</span>}
+                                        </span>
+                                    </div>
+                                </Link>
+                            </li>
+                        );
+                    })}
+            </ul>
+        </>
     );
 }
