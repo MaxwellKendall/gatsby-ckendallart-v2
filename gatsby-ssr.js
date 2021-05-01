ab213ae016c899fb0039e677ff3fc70e69a4c96d
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2021-05-01 13:09:03 -0400

    performance upgrade? removing unused dependencies

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,7 +4,7 @@ import React from 'react';
 const LoadScripts = () => {
     return (
         <>
-            <script src={`https://www.google.com/recaptcha/api.js?render=${GATSBY_RECAPTCHA_ID}`}></script>
+            <script src={`https://www.google.com/recaptcha/api.js?render=${GATSBY_RECAPTCHA_ID}`} async></script>
             <script dangerouslySetInnerHTML={{
                 __html: `(function(w, d, s, l, i) {
                     w[l] = w[l] || [];
@@ -15,19 +15,14 @@ const LoadScripts = () => {
                         j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
                             'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
                 })(window, document, 'script', 'dataLayer', '${GATSBY_GTM_ID}')`
-            }} />
+            }} async />
             <script dangerouslySetInnerHTML={{
                 __html: `((c, l, a, r, i, t, y) => {
                     c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
                     t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
                     y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
                 })(window, document, 'clarity', 'script', '${GATSBY_CLARITY_ID}')`
-            }} />
-            <script dangerouslySetInnerHTML={{
-                __html: `((n) => {
-                    console.log('hey' + n +  '!!!!!')
-                })('max')`
-            }} />
+            }} async />
         </>
     );
 }
