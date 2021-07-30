ce136503e6402170e3f237f2d76f63590533437c
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2021-07-30 14:28:31 -0400

    bug: variant with "do not track product inventory"
    should be treated as always for sale. Before these changes, they
    were showing sold out unless they were fulfilled by printful.
    
    Now claire is using another print on demand service for fulfillment.

--- client.js
+++ client.js
@@ -22,9 +22,10 @@ export const fetchProductInventory = (variantId) => {
     }
   })
   .then(async (data) => {
-    const { body: { variant: { inventory_quantity: remainingInventory, fulfillment_service }} } = await data.json();
+    const { body: { variant: { inventory_quantity: remainingInventory, fulfillment_service, inventory_management }} } = await data.json();
     console.info('number of variants remaining', remainingInventory);
-    if (fulfillment_service === 'printful') return 999;
+    const isInfiniteInventory = (inventory_management === null)
+    if (fulfillment_service === 'printful' || isInfiniteInventory) return 999;
     return remainingInventory;
   })
   .catch((e) => {
